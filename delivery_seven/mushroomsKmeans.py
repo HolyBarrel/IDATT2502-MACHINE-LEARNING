abc2f7d66d19464665401e5f517bf0a475edf6c0
HolyBarrel <magnus.allison@getmail.no>
2023-10-12 18:08:14 +0200

    Minor tweaks to delivery 7

--- delivery_seven/mushroomsKmeans.py
+++ delivery_seven/mushroomsKmeans.py
@@ -4,6 +4,7 @@ import pandas as pd
 from mpl_toolkits.mplot3d import axes3d, art3d
 
 data = pd.read_csv('agaricus-lepiota.data', sep=',', header=0, engine='python')
+# data.pop('edibility')
 
 from sklearn import metrics
 from sklearn.cluster import KMeans
@@ -24,7 +25,7 @@ X_reduced = pca.fit_transform(X_encoded.toarray())
 def compute_silhouette_scores(X, k_values):
     scores = []
     for k in k_values:
-        kmeans = KMeans(n_clusters=k, n_init=10).fit(X)
+        kmeans = KMeans(n_clusters=k).fit(X)
         score = silhouette_score(X, kmeans.labels_)
         scores.append(score)
     return scores
@@ -44,10 +45,16 @@ plot_silhouette_scores(k_values, silhouette_scores)
 
 optimal_k = k_values[silhouette_scores.index(max(silhouette_scores))]
 
-kmeans = KMeans(n_clusters=optimal_k, n_init=10).fit(X_reduced)
+print(f'Optimal k: {optimal_k}')
+
+kmeans = KMeans(n_clusters=optimal_k).fit(X_reduced)
+cluster_centers = kmeans.cluster_centers_
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=kmeans.labels_, cmap='cool')
+ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=kmeans.labels_, cmap='cool', alpha=0.05)
+ax.scatter(cluster_centers[:, 0], cluster_centers[:, 1], cluster_centers[:, 2], c='black', marker='x', s=100, label='Cluster Centers')
+
 ax.set_title(f'3D Visualization with {optimal_k} Clusters')
+ax.legend()
 plt.show()
